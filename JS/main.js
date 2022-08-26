b5c0adc7cf3f57b65e6e72a1f9fdee2e479b6b10
hosseinghadirzadeh <hosseinghadirzadeh81@gmail.com>
2022-08-26 23:18:47 +0430

    cardWriter added

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+function _write(e) {
+    let temp = e.target.value;
+    let show = document.getElementById('CN');
+    if (temp.length == 19) {
+        document.getElementById('Name').focus();
+    }
+    show.innerHTML = temp;
+    if (e.keyCode != 8)
+        if (show.innerText.length == 4 || show.innerText.length == 9 || show.innerText.length == 14) {
+            e.target.value += ' '
+        }
+}
